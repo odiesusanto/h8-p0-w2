466bb8d5f9d4a187697e25b3c72e002c19911eac
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-01 18:15:14 +0700

    rename exercise 3

--- exercise-3game.js
+++ exercise-3game.js
@@ -2,23 +2,23 @@ var nama = '';
 var peran = '';
 
 
-if(nama === 'Odie' && peran === ''){
+if(nama === '' && peran === ''){
     console.log("Pilih Peranmu untuk memulai game");
 }
 else if(peran === 'Ksatria'){
     console.log("Selamat datang di Proxytia!");
     console.log(`Halo Ksatria ${nama}, kamu dapat menyerang lawanmu.`);
 }
-else if(peran === 'Tabib'){
+else if(peran == 'Tabib'){
     console.log("Selamat datang di Proxytia!");
     console.log(`Halo Tabib ${nama}, kamu dapat menymbuhan yang terluka.`);
 }
-else if(peran === 'Penyihir'){
+else if(peran == 'Penyihir'){
     console.log("Selamat datang di Proxytia!");
     console.log(`Halo Penyihir ${nama}, keajaibanmu akan membantu untuk melawan musuhmu.`);
 }
 else{
-  console.log("Pilih nama dan peran untuk memulai game");
+  console.log("Invalid");
 }
 
 
