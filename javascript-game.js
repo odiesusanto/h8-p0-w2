c03bc01ac181a95e0c8d2faa805001e1fcb01d1c
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-01 15:06:56 +0700

    made edits to exercise 3

--- javascript-game.js
+++ javascript-game.js
@@ -9,11 +9,11 @@ else if(peran === 'Ksatria'){
     console.log("Selamat datang di Proxytia!");
     console.log(`Halo Ksatria ${nama}, kamu dapat menyerang lawanmu.`);
 }
-else if(peran == 'Tabib'){
+else if(peran === 'Tabib'){
     console.log("Selamat datang di Proxytia!");
     console.log(`Halo Tabib ${nama}, kamu dapat menymbuhan yang terluka.`);
 }
-else if(peran == 'Penyihir'){
+else if(peran === 'Penyihir'){
     console.log("Selamat datang di Proxytia!");
     console.log(`Halo Penyihir ${nama}, keajaibanmu akan membantu untuk melawan musuhmu.`);
 }
