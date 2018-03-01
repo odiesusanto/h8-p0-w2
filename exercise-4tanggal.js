f478ec82d9b421119218574088a87d032445a849
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-01 18:04:00 +0700

    exercise 4 format tanggal switch case

---
+++ exercise-4tanggal.js
@@ -0,0 +1,132 @@
+var tanggal = 1;
+var bulan = 5;
+var tahun = 1945;
+
+switch (tanggal){
+  case 1:
+    console.log(1);
+    break;
+  case 2:
+    console.log(2);
+    break;
+  case 3:
+    console.log(3);
+    break;
+  case 4:
+    console.log(4);
+    break;
+  case 5:
+    console.log(5);
+    break;
+  case 6:
+    console.log(6);
+    break;
+  case 7:
+    console.log(7);
+    break;
+  case 8: 
+    console.log(8);
+    break;
+  case 9:
+    console.log(9);
+    break;
+  case 10:
+    console.log(10);
+    break;
+  case 11:
+    console.log(11);
+    break;
+  case 12:
+    console.log(12);
+    break;
+  case 13:
+    console.log(13);
+    break;
+  case 14: 
+    console.log(14);
+    break;
+  case 15:
+    console.log(15);
+    break;
+  default:
+    date = 'unknown';
+}
+
+switch (bulan){
+  case 1:
+    console.log('Januari');
+    break;
+  case 2:
+    console.log('Februari');
+    break;
+  case 3:
+    console.log(Maret);
+    break;
+  case 4:
+    console.log('April');
+    break;
+  case 5:
+    console.log('Mei');
+    break;
+  case 6:
+    console.log(Juni);
+    break;
+  case 7:
+    console.log('Juli');
+    break;
+  case 8:
+    console.log('Agustus');
+    break;
+  case 9:
+    console.log('September');
+    break;
+  case 10:
+    console.log('Oktober');
+    break;
+  case 11:
+    console.log('Nopember');
+    break;
+  case 12:
+    console.log('Desember');
+    break;  
+  default:
+    month = 'unknown';
+}
+
+switch (tahun){
+  case 1940:
+    console.log(1940);
+    break;
+  case 1941:
+    console.log(1941);
+    break;
+  case 1942:
+    console.log(1942);
+    break;
+  case 1943:
+    console.log(1943);
+    break;
+  case 1944:
+    console.log(1944);
+    break;
+  case 1945:
+    console.log(1945);
+    break;
+  case 1946:
+    console.log(1946);
+    break;
+  case 1947:
+    console.log(1947);
+    break;
+  case 1948:
+    console.log(1948);
+    break;
+  case 1949:
+    console.log(1949);
+    break;
+  case 1950:
+    console.log(1950);
+    break;
+  default:
+    tahun = 'unknown';
+}
