237785af5e5a4d99706b10c696361d716024ef87
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-04 01:37:10 +0700

    exercise 9 functions

---
+++ exercise-9functions.js
@@ -0,0 +1,19 @@
+1.
+function shoutOut () {
+  return 'Halo Function!';
+}
+console.log(shoutOut());
+
+
+2.
+function calculateMultiply(number1, number2) {
+  return number1 * number2;
+}
+console.log(calculateMultiply(5,6));
+
+
+3.
+function processSentence(name, age, address, hobby) {
+  console.log('Nama saya ' + name + ', umur saya ' + age + ' tahun, alamat saya di ' + address + ', dan saya punya hobby yaitu ' + hobby + '!');
+}
+processSentence('Agus', 30, 'Jln. Malioboro, Yogjakarta','gaming');
