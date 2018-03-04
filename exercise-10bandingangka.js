8356b0707b4b3bdea23a3bbf26e7d97b87a15e7b
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-04 22:24:35 +0700

    exercise 10 bandingkan angka

---
+++ exercise-10bandingangka.js
@@ -0,0 +1,23 @@
+function bandingkanAngka(angka1, angka2){
+  if (angka2 > angka1) {
+    return true;
+  } else if (angka2 === angka1) {
+    return -1;
+  } else {
+    return false;
+  }
+}
+
+bandingkanAngka(2,7);
+bandingkanAngka(3,1);
+bandingkanAngka(5,5);
+bandingkanAngka(6,15);
+bandingkanAngka(10,4);
+bandingkanAngka(7,7);
+
+console.log(bandingkanAngka(2,7));
+console.log(bandingkanAngka(3,1));
+console.log(bandingkanAngka(5,5));
+console.log(bandingkanAngka(6,15));
+console.log(bandingkanAngka(10,4));
+console.log(bandingkanAngka(7,7));
