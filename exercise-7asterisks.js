08d07b89981a6353b69b0904d240beab14aa99ee
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-05 13:35:15 +0700

    edit exercise 7 after feedback

--- exercise-7asterisks.js
+++ exercise-7asterisks.js
@@ -5,28 +5,23 @@ for(rows = 1; rows <= 5; rows ++){
 
 
 2.
-for(rows2 = 5; rows2 <= 5; rows2 ++){
+for(rows = 1; rows <= 5; rows ++){
+  console.log('*');
+}
+for(rows2 = 0; rows2 < 5; rows2 ++){
+  var asterisk = '';
   for(rows = 0; rows < 5; rows ++){
-  console.log('*****');
+  asterisk += '*';
   }
+  console.log(asterisk);
 }
 
 
 3.
-for(rows3 = 5; rows3 <= 5; ++rows3){
-  for(rows4 = 1; rows4 <= 1; ++rows4){
-    console.log('*');
-    for(rows5 = 1; rows5 <= 1; ++rows5){
-      console.log('**');
-      for(rows6 = 1; rows6 <= 1; ++rows6){
-        console.log('***');
-        for(rows7 = 1; rows7 <= 1; ++rows7){
-          console.log('****');
-          for(rows8 = 1; rows8 <= 1; ++rows8){
-            console.log('*****');
-          }
-        }
-      }
-    }
+for(rows3 = 0; rows3 < 5; rows3++){
+  var asterisk = '';
+  for(row = 0; row <= rows3; row++){
+    asterisk += '*';
   }
+  console.log(asterisk);
 }
